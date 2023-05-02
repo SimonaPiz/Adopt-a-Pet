11c37c2290293f694c8da7db3e6760b4a701941d
Simona Pizio <simona.pizio@gmail.com>
2023-05-02 16:31:30 +0200

    Import RouterProvider from react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
+import { RouterProvider } from 'react-router-dom';
 // create router with JSX Route elements
 const appRouter = "REPLACE ME";
 
