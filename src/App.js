28be53ad19d8603aa886cdacbb3c8ab93dcf40c2
Simona Pizio <simona.pizio@gmail.com>
2023-05-02 16:59:49 +0200

    in App - Add first route /

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Root from './components/root';
 // Add react-router-dom imports
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(<Route path='/' element={ <Root/> }></Route>));
 
 function App() {
   return (
