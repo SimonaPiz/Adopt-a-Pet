ebabdcf43ca1102bc48aeb1bb8a6b3eb22dc3b95
Simona Pizio <simona.pizio@gmail.com>
2023-05-03 10:05:15 +0200

    in App - nest index route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Root from './components/root';
 // Add react-router-dom imports
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(<Route path='/' element={ <Root/> }></Route>));
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={ <Root/> }>
+    <Route index element={ <HomePage/> }></Route>
+  </Route>
+));
 
 function App() {
   return (
