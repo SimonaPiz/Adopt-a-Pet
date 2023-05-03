f4a1fd3d06a6a8734b0384b01678801d350a010d
Simona Pizio <simona.pizio@gmail.com>
2023-05-03 10:37:04 +0200

    in pages\detail\index.js - import and add useParam() function for set type\id of page, PetDetailsPage

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={ <Root/> }>
-    <Route index element={ <HomePage/> }></Route>
-    <Route path=':type' element={ <HomePage/> }></Route>
+    <Route index element={ <HomePage/> }/>
+    <Route path=':type' element={ <HomePage/> }/>
+    <Route path=':type/:id' element={ <PetDetailsPage/> }/>
   </Route>
 ));
 
